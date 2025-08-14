030e690608fcbada602b65f09c5502efed795e60
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-08-14 08:50:12 +0530

    typer initialization

---
+++ cli.py
@@ -0,0 +1,12 @@
+import typer
+app = typer.Typer(help="Hey niqqas")
+@app.command()
+def hello(name: str):
+    """
+    Greets the user with their name.
+    """
+    print("Hello, {name}! Welcome to the DevFlow AI v0.")
+
+if __name__ == "__main__":
+    app()
+    
